f817b64fb0b21ebbdd9ef7cc390f73a20ee06a31
kitaharasetusna <tlou2sucks@163.com>
2023-11-29 13:23:15 -0500

    update plotting mnist

---
+++ examples/plot_mnist.py
@@ -0,0 +1,33 @@
+import pickle
+import sys
+sys.path.append('..')  # Adds the parent directory to the Python path1
+import matplotlib.pyplot as plt
+from my_utils.utils_reading_disks import get_dict_from_yaml
+
+
+
+path_config = '../configs/4_mnist_sra_fl_non_iid.yaml'
+configs = get_dict_from_yaml(path=path_config)
+print(configs)
+
+folder_idx = '../idx_'+configs['exp_name']
+with open(folder_idx+'/idxs_'+str(configs['degree_non_iid'])+'.pkl', 'rb') as f:
+    data_dict = pickle.load(f)
+    f.close()
+
+with open('../idx_'+configs['exp_name']+'_accs.pkl', 'rb') as f:
+    test_accuracy = pickle.load(f) 
+    f.close()
+print(test_accuracy)
+
+train_accs =[]
+test_accs = []
+for tarin_acc, test_acc in test_accuracy:
+    test_accs.append(test_acc)
+print(train_accs)
+print(test_accs)
+
+
+plt.figure(figsize=(5, 4))
+plt.plot(test_accs)
+plt.show()
